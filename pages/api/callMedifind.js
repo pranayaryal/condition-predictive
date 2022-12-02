1543c3acaf0b719793eb5299591761f4e67f7f01
pranay <drpranayaryal@gmail.com>
2022-12-01 23:26:36 -0500

    corr er

--- pages/api/callMedifind.js
+++ pages/api/callMedifind.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const callMedifind = async(req, res) {
+const callMedifind = async(req, res) => {
   axios.get("https://www.medifind.com/api/autocomplete/magic?input=ae")
   .then( resp => {
     console.log(resp)
